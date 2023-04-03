cf8f8a6b4ccb2a6ef74e4e286fad5cc9c511902e
Norman Hong <norman.hong235@gmail.com>
2023-04-03 13:20:40 -0400

    Moved job declaration and definition out of ZetaSession file

--- ZetaSession.cpp
+++ ZetaSession.cpp
@@ -1,5 +1,6 @@
 #include "Algebra.h"
 #include "DTypes.h"
+#include "Job.h"
 #include <any>
 #include <iostream>
 #include <queue>
@@ -8,56 +9,7 @@
 #include <variant>
 #include <vector>
 
-template <Numeric T> class Job {
 
-public:
-  Job(T (*func_ptr)(const T *), T *arg1); // Function takes a single pointer of any type T and returns the same type.
-  Job(T (*func_ptr)());                   // Function takes no argument and returns type T.
-  Job(T (*func_ptr)(const T *, const T *), T *arg1, T *arg2);
-  Job(const Job<T> &other);
-  Job &operator=(const Job<T> &a);
-  ~Job();
-  virtual T Run();
-
-private:
-  std::function<T(const T *, const T *)> _func_ptr_2_args;
-  std::function<T(const T *)> _func_ptr;
-  std::function<T()> _func_ptr_no_args;
-  const T *_args;
-  const T *_args2;
-};
-
-template <Numeric T> Job<T>::Job(T (*func_ptr)(const T *), T *arg1) : _func_ptr{func_ptr}, _args{arg1}, _args2{NULL} {}
-template <Numeric T> Job<T>::Job(T (*func_ptr)()) : _func_ptr_no_args{func_ptr}, _args{NULL}, _args2{NULL} {}
-template <Numeric T> Job<T>::Job(T (*func_ptr)(const T *, const T *), T *arg1, T *arg2) : _func_ptr_2_args{func_ptr}, _args{arg1}, _args2{arg2} {}
-template <Numeric T>
-Job<T>::Job(const Job<T> &other)
-    : _args2{new T{*other._args2}}, _func_ptr{other._func_ptr}, _func_ptr_no_args{other._func_ptr_no_args}, _func_ptr_2_args{other._func_ptr_2_args} {
-  _args = new T{*other._args};
-}
-template <Numeric T> Job<T> &Job<T>::operator=(const Job<T> &other) {
-  delete _args;
-  _args = new T{*other._args};
-  delete _args2;
-  _args2 = new T{*other._args2};
-  _func_ptr = other._func_ptr;
-  _func_ptr_2_args = other._func_ptr_2_args;
-  _func_ptr_no_args = other._func_ptr_no_args;
-}
-
-template <Numeric T> Job<T>::~Job() {
-  delete _args;
-  delete _args2;
-}
-template <Numeric T> T Job<T>::Run() {
-  // Executes function and pass in arguments.
-  if (_args && _args2) {
-    return (_func_ptr_2_args)(_args, _args2);
-  } else if (_args) {
-    return (_func_ptr)(_args);
-  } else
-    return (_func_ptr_no_args)();
-}
 
 template <> class Job<NumericVariant> {
 public:
