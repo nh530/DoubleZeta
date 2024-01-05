ecf8e91edd6ba99248264fa69b87edc0847916d8
Norman Hong <norman.hong235@gmail.com>
2024-01-05 10:01:12 -0500

    fixed mem leak in benchmarking script

--- examples/benchmark/parallel_math.cpp
+++ examples/benchmark/parallel_math.cpp
@@ -7,13 +7,13 @@
 #include <iostream>
 #include <list>
 
-ZetaSession newZeta{10};
+ZetaSession newZeta{12};
 /* N_SMPLS simulates the number of tasks to run. For example, N_SMPLS = 10 means there are 10 tasks created in the threadpool.
  * LEN_ARRAY simulates the number of values in a fake summation. For example, LEN_ARRAY = 100 means to execute a summation 100 * 6 times.
  *
  * */
-int N_SMPLS = 500;
-int LEN_ARRAY = 500;
+int N_SMPLS = 5;
+int LEN_ARRAY = 50;
 
 float long_add() {
   float out = 0.0f;
@@ -42,7 +42,7 @@ float long_add() {
 
 std::chrono::duration<double, std::milli> base_trial() {
   auto ti1 = std::chrono::high_resolution_clock::now();
-  int val;
+  int val = 0;
   for (int i = 0; i < N_SMPLS; i++) {
     val = long_add();
   }
@@ -55,7 +55,7 @@ std::chrono::duration<double, std::milli> benchmark2() {
   auto t1 = std::chrono::high_resolution_clock::now();
   float out = 0;
   float temp = 4809.1111;
-  std::list<Status<float>> out_arr;
+  std::list<Status<float>> out_arr{};
   for (int i = 0; i < 100000; i++) {
     out_arr.push_back(std::move(newZeta.SubmitTask(&add, temp, temp)));
   }
@@ -75,15 +75,13 @@ std::chrono::duration<double, std::milli> benchmark2() {
 std::chrono::duration<double, std::milli> benchmark3() {
   auto t1 = std::chrono::high_resolution_clock::now();
   float out = 0;
-  std::list<Status<float>> out_arr;
+  std::list<Status<float> *> out_arr{};
   for (int i = 0; i < N_SMPLS; i++) {
-    out_arr.push_back(std::move(newZeta.SubmitTask(&long_add)));
+		Status<float> *temp = new Status<float>(newZeta.SubmitTask(&long_add)); // note pointer temp is a local variable.
+    out_arr.push_back(temp);
   }
-  while (true) {
-    if (newZeta.Busy())
-      continue;
-    else
-      break;
+  while (newZeta.Busy()) {
+		continue;
   }
   auto t2 = std::chrono::high_resolution_clock::now();
   return std::chrono::duration<double, std::milli>{t2 - t1};
@@ -103,9 +101,10 @@ int main() {
     sum += times[i].count();
   }
   std::cout << "Serial calculation runtime: " << sum / 10 << '\n';
-  sum = 0;
+  double summ = 0;
   for (int i = 0; i < 10; i++) {
-    sum += trials[i].count();
+    summ += trials[i].count();
   }
-  std::cout << "Multi-threaded calculation runtime: " << sum / 10 << '\n';
+  std::cout << "Multi-threaded calculation runtime: " << summ / 10 << '\n';
+	std::cout << "Fractional improvement: " << sum / summ << '\n';
 }
